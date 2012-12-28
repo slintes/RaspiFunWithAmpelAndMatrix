0b4e0db71cb6b497083f423fd26cb4a64261eec9
marc <marc@slintes.net>
2012-12-29 00:09:18 +0100

    "randomized" color, use blinkrate and brightness

--- src/main/java/net/slintes/raspi/RaspiFunAmpelAndMatrix.java
+++ src/main/java/net/slintes/raspi/RaspiFunAmpelAndMatrix.java
@@ -24,13 +24,16 @@ public class RaspiFunAmpelAndMatrix {
     public static void main(String[] args) throws IOException {
 
         Adafruit8x8LEDMatrix leds = new Adafruit8x8LEDMatrix(I2C_BUS_NR, LED_PACK_ADDRESS);
+        leds.setBlinkRate(AdafruitLEDBackPack.BlinkRate.TWO_HZ);
+
         Ampel ampel = new Ampel(GPIO_PIN_RED, GPIO_PIN_YELLOW, GPIO_PIN_GREEN);
 
         while(true){
             for (int row = 0; row < 8; row++) {
                 for (int col = 0; col < 8; col++) {
 
-                    int color = row % 3;
+                    // get some "random" color
+                    int color = (row/2 + col/3) % 3;
 
                     Adafruit8x8LEDMatrix.LedColor ledColor;
                     Ampel.State ampelState;
@@ -42,19 +45,34 @@ public class RaspiFunAmpelAndMatrix {
                         default: ledColor= Adafruit8x8LEDMatrix.LedColor.OFF; ampelState = Ampel.State.OFF;
                     }
 
-                    ampel.setState(ampelState);
+                    // playing with blinkrate in order to switch display off sometimes
+                    if(((row + col) % 2) == 0){
+                        leds.setBlinkRate(AdafruitLEDBackPack.BlinkRate.DISPLAY_OFF);
+                        ampel.setState(Ampel.State.OFF);
+                    }
+                    else{
+                        leds.setBlinkRate(AdafruitLEDBackPack.BlinkRate.BLINK_OFF);
+                        ampel.setState(ampelState);
+                    }
+
+                    // set brightness
+                    leds.setBrightness(row * 2);
 
                     leds.clear(false);
                     leds.setPixel(row, row % 2 == 0 ? col : 7-col, ledColor);
                     leds.writeDisplay();
 
                     try {
-                        Thread.sleep(80);
+                        Thread.sleep(10);
                     } catch (InterruptedException e) {
-                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                        e.printStackTrace();
                     }
+
                 }
             }
+
         }
+
     }
+
 }
