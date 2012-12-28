822fe0c29d08b8c051c428cb583846931bf6ed99
marc <marc@slintes.net>
2012-12-28 16:37:23 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,16 @@
+Raspberry Pi + some LEDs + Adafruit LED Backpack + Java + Pi4J = Fun :)
+
+see it in action at https://www.youtube.com/watch?v=0miZit94TEU
+
+Links:
+
+* http://learn.adafruit.com/adafruit-raspberry-pi-educational-linux-distro/
+* http://jdk8.java.net/fxarmpreview/index.html
+* http://pi4j.com/index.html
+* http://learn.adafruit.com/adafruit-led-backpack/overview
+* http://learn.adafruit.com/matrix-7-segment-led-backpack-with-the-raspberry-pi/overview
+* https://github.com/adafruit/Adafruit-LED-Backpack-Library
+* https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code/tree/master/Adafruit_LEDBackpack
+
+* https://github.com/slintes/RaspiAmpel
+* https://github.com/slintes/RaspiAdafruitLEDBackpack
---
+++ src/main/java/net/slintes/raspi/RaspiFunAmpel.java
@@ -0,0 +1,58 @@
+package net.slintes.raspi;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: slintes
+ * Date: 28.12.12
+ * Time: 14:02
+ *
+ * a simple Ampel (traffic light) made out of 3 LEDs connected to 3 GPIO pins of a Raspberry Pi
+ *
+ */
+public class RaspiFunAmpel {
+
+    // see http://pi4j.com/usage.html#Pin_Numbering
+    private static final int GPIO_PIN_RED = 4;
+    private static final int GPIO_PIN_YELLOW = 5;
+    private static final int GPIO_PIN_GREEN = 6;
+
+    public static void main(String[] args) {
+
+
+        Ampel ampel = new Ampel(GPIO_PIN_RED, GPIO_PIN_YELLOW, GPIO_PIN_GREEN);
+
+        int durationLong = 2000;
+        int durationShort= 800;
+
+        StateAndDuration[] stateAndDurations = {
+                new StateAndDuration(Ampel.State.RED, durationLong),
+                new StateAndDuration(Ampel.State.RED_YELLOW, durationShort),
+                new StateAndDuration(Ampel.State.GREEN, durationLong),
+                new StateAndDuration(Ampel.State.YELLOW, durationLong)};
+
+        while(true){
+            for (int i=0, length=stateAndDurations.length; i<length; i++){
+
+                StateAndDuration stateAndDuration = stateAndDurations[i];
+                ampel.setState(stateAndDuration.state);
+
+                try {
+                    Thread.sleep(stateAndDuration.duration);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class StateAndDuration {
+
+        Ampel.State state;
+        int duration;
+
+        StateAndDuration(Ampel.State state, int duration) {
+            this.state = state;
+            this.duration = duration;
+        }
+    }
+}
